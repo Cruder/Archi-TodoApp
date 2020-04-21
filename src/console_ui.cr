afcc4b2414e3c28c273f0d4058969ce28e2452b2
Amelie Certin <amelie.certin@gmail.com>
2020-04-21 23:19:43 +0200

    Input method can be replace in specs

--- src/console_ui.cr
+++ src/console_ui.cr
@@ -1,38 +1,38 @@
-
-abstract class IInput
-    abstract def read
-end
-
 abstract class IOutput
-    abstract def send(message : String)
-    abstract def help
+  abstract def send(message : String)
+  abstract def help
 end
 
-class ConsoleOutput < IOutput
-    def send(message)
-        puts message
-    end
+class Console::Output < IOutput
+  def send(message)
+    puts message
+  end
 
-    def help
-        puts "You can do :"
-        puts "new <content todo> : "
-        puts "rem <id-todo>"
-        puts "list"
-        puts "complete <id-todo>"
-    end
+  def help
+    puts "You can do :"
+    puts "new <content todo> : "
+    puts "rem <id-todo>"
+    puts "list"
+    puts "complete <id-todo>"
+  end
 end
 
-class ConsoleInput < IInput
-    def read
-        gets
-    end
+class Console::Input
+  @@catch = -> { gets }
+
+  def self.set(new_catch : Proc(String?))
+    @@catch = new_catch
+  end
+
+  def self.read : String?
+    @@catch.try(&.call) || gets
+  end
 end
 
-input = ConsoleInput.new
-output = ConsoleOutput.new
+output = Console::Output.new
 
 output.send("Enter something")
-smth = input.read
+smth = Console::Input.read
 
 
 
