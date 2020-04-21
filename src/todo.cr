fa4be40657108000e13fbdf30f6adc8029892cf7
Amelie Certin <amelie.certin@gmail.com>
2020-04-22 00:39:11 +0200

    Use parser

---
+++ src/todo.cr
@@ -0,0 +1,27 @@
+require "option_parser"
+require "./project"
+
+repo = TaskRepository.new
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: todo [arguments]"
+  parser.on("-l", "--list", "List active todos") do
+    tasks = repo.active
+    tasks.each do |task|
+      puts task.to_string(TaskSpanFormatter.new)
+    end
+  end
+  parser.on("-a NAME", "--add=NAME", "Add a new todo") do |name|
+    task = Task.new(name)
+    repo.insert(task)
+  end
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
