1c0bbb4a37f83f5ebf1736502153c6d6fd52bf5a
csgrdcelia <casagrande.celia@gmail.com>
2020-04-22 13:04:48 +0200

    test add, remove, list activities

--- spec/controller_spec.cr
+++ spec/controller_spec.cr
@@ -77,13 +77,13 @@ end
 
 describe DoneTaskActivity do
   after_each { TaskRepository.flush }
-  
+
   context "can mark task as done" do
     it "should mark task as done when entering task id" do
       repo = TaskRepository.new
       repo.insert(Task.new("test task"))
       task = repo.all.last
-      Logger.new(STDOUT).info("id : #{task.id}")
+
       output_catcher = IO::Memory.new
       controller = Controller.new(output_catcher)
       controller.register("main") { |ctrl| MainActivity.new(ctrl) }
@@ -118,3 +118,126 @@ describe DoneTaskActivity do
   end
 end
 
+describe RemoveTaskActivity do
+  after_each { TaskRepository.flush }
+  
+  context "can remove task" do
+    it "should remove task when entering task id" do
+      repo = TaskRepository.new
+      repo.insert(Task.new("test task"))
+      task = repo.all.last
+      
+      output_catcher = IO::Memory.new
+      controller = Controller.new(output_catcher)
+      controller.register("main") { |ctrl| MainActivity.new(ctrl) }
+      controller.register("remove_task") { |ctrl| RemoveTaskActivity.new(ctrl) }
+      controller.push("main")
+
+      fake_input = IO::Memory.new("r\n#{task.id}\nq")
+      controller.run(fake_input)
+
+      output_catcher.to_s.should eq(
+        <<-TXT
+        Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit
+        
+        Task ID > Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit
+
+        TXT
+      )
+
+      repo.all.should eq([] of Task)
+    end
+  end
+end
+
+describe AddTaskActivity do
+  after_each { TaskRepository.flush }
+  
+  context "can add task" do
+    it "should add task when entering task name" do
+      repo = TaskRepository.new
+      output_catcher = IO::Memory.new
+      controller = Controller.new(output_catcher)
+      controller.register("main") { |ctrl| MainActivity.new(ctrl) }
+      controller.register("add_task") { |ctrl| AddTaskActivity.new(ctrl) }
+      controller.push("main")
+
+      fake_input = IO::Memory.new("a\ntest\nq")
+      controller.run(fake_input)
+
+      output_catcher.to_s.should eq(
+        <<-TXT
+        Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit
+        
+        Name your task > Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit
+
+        TXT
+      )
+
+      repo.all.last.name.should eq("test")
+    end
+  end
+end
+
+describe ListTasksActivity do
+  after_each { TaskRepository.flush }
+  
+  context "can list tasks" do
+    it "should list tasks when entering l" do
+      ApplicationTime.set ->{ Time.utc(2010, 1, 3) }
+      repo = TaskRepository.new
+      task = Task.new(1, "test", Time.utc(2010, 1, 1), false)
+      repo.insert(task)
+      task = repo.all.last
+      output_catcher = IO::Memory.new
+      controller = Controller.new(output_catcher)
+      controller.register("main") { |ctrl| MainActivity.new(ctrl) }
+      controller.register("list_tasks") { |ctrl| ListTasksActivity.new(ctrl) }
+      controller.push("main")
+
+      fake_input = IO::Memory.new("l\nq\nq")
+      controller.run(fake_input)
+
+      output_catcher.to_s.should eq(
+        <<-TXT
+        Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit
+        ##{task.id} - [2d] test
+        
+        Press [ENTER] to continue
+        
+        Menu -
+        l - List Tasks
+        a - Add Task
+        r - Remove Task
+        d - Mark task as done
+        q - Quit\n
+        TXT
+      )
+    end
+  end
+end
