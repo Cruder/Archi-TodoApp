51995a13dd1b1f35479fd9a40d89e68ee9f8485c
Barogthor <f.thery3103@gmail.com>
2020-04-22 10:26:26 +0200

    Repository abstraction

--- src/repository.cr
+++ src/repository.cr
@@ -1,3 +1,10 @@
 abstract class Repository
     abstract def all
+    abstract def self.flush
+    abstract def all
+    abstract def active
+    abstract def insert(task : Task)
+    abstract def remove(id)
+    abstract def complete(id)
+    abstract def find(id)
 end
